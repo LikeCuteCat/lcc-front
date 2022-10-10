c44274545ee839cb14526996737b210667f082bd
Spencer Kwon <spencerkwon92@gmail.com>
2022-10-10 04:16:37 -0600

    Created component

---
+++ src/components/ListForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="wrapper">
+    <h3>{{ title }}</h3>
+    <h3>{{ date }}</h3>
+  </div>
+  <hr />
+</template>
+
+<script lang="ts">
+export default {
+  props: ["title", "date"],
+};
+</script>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  align-items: center;
+}
+hr {
+  color: hsl(0, 0%, 90%);
+}
+</style>
--- src/views/Notice/NoticeList.vue
+++ src/views/Notice/NoticeList.vue
@@ -1,9 +1,26 @@
 <template>
-	<div>
-		<h2>Notice List Page</h2>
-	</div>
+  <div>
+    <h2>Announcements</h2>
+    <ListForm :title="Kwon" :date="2020" />
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script>
+import { defineComponent } from "vue";
+import ListForm from "../../components/ListForm";
+export default defineComponent({
+  components: {
+    ListForm,
+  },
+});
+</script>
 
-<style scoped></style>
+<style scoped>
+div {
+  margin: auto;
+  width: 70%;
+  border: solid 2px gray;
+  border-radius: 10px;
+  height: 100vh;
+}
+</style>
