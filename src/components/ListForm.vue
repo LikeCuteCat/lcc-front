e85a111f4c0cc64feed3e9d25b6b571ba5078988
Spencer Kwon <spencerkwon92@gmail.com>
2022-10-15 03:44:56 -0600

    Organized the Noti Page with issue.

--- src/components/ListForm.vue
+++ src/components/ListForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="wrapper">
+  <div class="wrapper" @click="clickHandle">
     <h3>{{ title }}</h3>
     <h3>{{ date }}</h3>
   </div>
@@ -8,16 +8,31 @@
 
 <script lang="ts">
 export default {
-  props: ["title", "date"],
+  props: { title: String, date: String },
+  methods: {
+    clickHandle() {
+      console.log("You Clicked!!!");
+    },
+  },
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .wrapper {
   display: flex;
   align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 0px 10px 0px 10px;
+
+  &:hover {
+    cursor: pointer;
+  }
+  h3 {
+    font-size: 15px;
+  }
 }
 hr {
-  color: hsl(0, 0%, 90%);
+  color: hsl(0, 0%, 50%);
 }
 </style>
--- src/views/Notice/NoticeList.vue
+++ src/views/Notice/NoticeList.vue
@@ -1,25 +1,52 @@
 <template>
   <div>
     <h2>Announcements</h2>
-    <ListForm :title="Kwon" :date="2020" />
+    <div v-for="notice in notices" :key="notice.id">
+      <ListForm :title="notice.title" :date="notice.createdAt" />
+    </div>
   </div>
 </template>
 
-<script>
-import { defineComponent } from "vue";
-import ListForm from "../../components/ListForm";
-export default defineComponent({
-  components: {
-    ListForm,
-  },
-});
+<script setup lang="ts">
+import { defineComponent, ref } from "vue";
+import axios from "axios";
+import ListForm from "../../components/ListForm.vue";
+
+import { getNotices } from "@/api/admin";
+import type { NoticeItem } from "@/types/Notice";
+
+const notices = ref<Array<NoticeItem>>([]);
+const fetchNotices = async () => {
+  try {
+    const { data } = await getNotices();
+    notices.value = data;
+    console.log(notices);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// export default defineComponent({
+//   components: { ListForm },
+//   // mounted() {
+//   //   axios
+//   //     .get("http://localhost:3001/notices")
+//   //     .then((res) => (this.notices = res.data))
+//   //     .catch((error) => console.log(error));
+//   // },
+//   // methods: {
+//   //   dataTest() {
+//   //     console.log(this.notices);
+//   //   },
+//   // },
+// });
+fetchNotices();
 </script>
 
 <style scoped>
 div {
   margin: auto;
   width: 70%;
-  border: solid 2px gray;
   border-radius: 10px;
   height: 100vh;
 }
